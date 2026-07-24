830e0a642015109954ed23b3d4b8c1f1c6808332
fullfightingbons <fullfightingbons@gmail.com>
2026-07-24 12:49:27 +0200

    Delete notification.js

--- notification.js
+++
@@ -1,8 +0,0 @@
-function isMissingSchemaError(error) {
-  const message = String(error?.message || error || "");
-  return /no such table|no such column/i.test(message);
-}
-
-export {
-  isMissingSchemaError,
-};
